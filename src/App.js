ffefca7a01b1d4a435b334c7e96d2c0f68316920
NurKevser <nurkevserkadiroglu@gmail.com>
2021-08-08 16:33:35 +0300

    search bar completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import JSONDATA from "./MOCK_DATA.json";
+import { useState } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="Search..." onChange={event => {setSearchTerm(event.target.value)}}/>
+      {JSONDATA.filter((val)=>{
+        if(searchTerm == ""){
+          return val
+        }else if(val.first_name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())){
+          return val
+        }
+      }).map((val, key) => {
+        return (
+        <div className="user" key={key}>
+          <p>{val.first_name}</p>
+        </div>
+        );
+      })}
     </div>
   );
 }
